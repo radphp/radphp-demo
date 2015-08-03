12d1cb7c265a01245b71e04e6ea7aea3a3a4af1e
Omid Mottaghi <omidmr@gmail.com>
2015-08-03 22:46:36 +0430

    add default master twig to app

--- src/App/Bootstrap.php
+++ src/App/Bootstrap.php
@@ -7,6 +7,7 @@ use Rad\Core\Bundle;
 use Rad\Routing\Middleware\DispatcherMiddleware;
 use Rad\Routing\Middleware\RouterMiddleware;
 use Rad\Routing\MiddlewareCollection;
+use Twig\Library\Helper as TwigHelper;
 
 /**
  * App Bootstrap
@@ -21,6 +22,9 @@ class Bootstrap extends Bundle
     public function startup()
     {
         parent::startup();
+
+        TwigHelper::addMasterTwig('@App/master.twig');
+
         if ('cli' !== PHP_SAPI) {
             MiddlewareCollection::getInstance()->add(new RouterMiddleware());
             MiddlewareCollection::getInstance()->add(new DispatcherMiddleware());
