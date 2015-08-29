5a2e86f7d343d8e04404efc066d9f7a2fe468789
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2015-08-29 22:25:13 +0430

    Change variable name

--- bin/provision.sh
+++ bin/provision.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-CWD=$1
+PROJECT_DIR=$1
 
 DB_NAME=radphp_db
 DB_USER=radphp_user
@@ -12,6 +12,6 @@ echo "CREATE USER $DB_USER WITH PASSWORD '$DB_PASS';" | sudo -u postgres psql
 echo "CREATE DATABASE $DB_NAME;" | sudo -u postgres psql
 echo "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME to $DB_USER;" | sudo -u postgres psql
 
-${CWD}/bin/console.php cake_orm:build
-${CWD}/bin/console.php migrations:migrate
-${CWD}/bin/console.php migrations:migrate -b Pages
+${PROJECT_DIR}/bin/console.php cake_orm:build
+${PROJECT_DIR}/bin/console.php migrations:migrate
+${PROJECT_DIR}/bin/console.php migrations:migrate -b Pages
